c52be31d8d5843414ebac404cfc4be54776e0a42
SAli <set.ali.mousavi@gmail.com>
2022-08-30 23:37:53 +0430

    Session 2

---
+++ index.js
@@ -0,0 +1,125 @@
+var friends = ['Ali', 'Eli', 'Qoli', 'Fati']
+
+console.log(friends)
+console.log(friends.length)
+console.log(friends[0])
+console.log(friends[1])
+console.log(friends[3])
+console.log(friends[4])
+
+var arr = [11, 'ali', true, () => {}, { name: 'Qoli' }, []]
+
+console.log(typeof arr)
+
+// // 74FDEA
+// var family = {
+//   father: 'Mohammad'
+// }
+
+// // 12BCAD
+// var obj1 = { name: 'Ali', age: 33, family: family }
+
+// // AB4CDA
+// var obj2 = { name: 'Ali', age: 34, family }
+
+// console.log(obj1 === obj2)
+// console.log(obj1.name === obj2.name)
+// console.log(obj1.family === obj2.family)
+// console.log(obj1.family.father === obj2.family.father)
+
+// // 12ED2A
+// var ali = {
+//   name: 'Ali',
+//   family: 'Mousavi'
+// }
+
+// console.log(ali)
+
+// // 12ED2A
+// var hasan = ali
+
+// console.log(hasan)
+
+// hasan.name = 'Hasan'
+
+// console.log(ali)
+// console.log(hasan)
+
+// console.log(ali === hasan)
+
+// const obj = {
+//   firstName: 'Ali',
+//   lastName: 'Mousavi',
+//   age: 33
+//   // family: {
+//   //   // father: {
+//   //   //   // name: 'Mohammad',
+//   //   //   family: 'Mousavi'
+//   //   // },
+//   //   mother: 'Hajar'
+//   // }
+// }
+
+// console.log(1)
+
+// console.log(obj.family?.father?.name)
+
+// console.log(2)
+
+// console.log(obj.firstName)
+// console.log(obj.lastName)
+// console.log(obj.age)
+
+// var propertyName = 'age'
+
+// console.log(obj[propertyName]) // Correct
+// console.log(obj.propertyName) // Wrong
+
+// var str = 'Salam  \uD83D\uDE00 ali'
+
+// console.log(str)
+
+// var a = 2 - 'Ali'
+
+// // if (a === NaN) { // Wrong
+// if (Number.isNaN(a)) { // Correct
+//   // Wrong
+//   // ...
+// }
+
+// camelCaseNaming    myFirstName
+// PascalCaseNaming   MyFirstName
+// snake_case_naming  my_first_name
+// kebab-case-naming  my-first-name
+// UPPER_CASE_NAMING  MY_FIRST_NAME
+
+// ` ~ ! @ # $ % ^ & * () - _ + = | / \ [] {} : ; , . ' " ? > <
+
+// var نام = 'علی'
+
+// console.log(نام)
+
+// var firstName = 'Ali'
+
+// var lastName
+
+// lastName = 'Mousavi'
+
+// console.log(firstName)
+// console.log(lastName)
+
+// lastName = 'Hasani'
+
+// console.log(lastName)
+
+// var age = 33,
+//   gender = 'male'
+
+// This is a log
+// console.log('Ali')
+
+/*
+console.log('Salam')
+
+console.log('hello')
+*/
