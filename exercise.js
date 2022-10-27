32a04d5ddab2f81b4111a767cfe6ba8310843787
SAli <set.ali.mousavi@gmail.com>
2022-10-27 19:43:18 +0330

    Session 13

--- exercise.js
+++ exercise.js
@@ -1,31 +1,141 @@
-var person = {
-  firstName: 'Ali',
-  lastName: 'Mousavi',
-  age: 33,
-  family: {
-    father: 'Mohammad',
-    mother: 'Hajar'
-  },
-  friends: ['Ali', 'Eli', 'Qoli', 'Fati']
+///////// 13
+
+// function power (a, b) {
+//   var result = a
+
+//   for (var i = 1; i < b; i++) {
+//     result *= a
+//   }
+
+//   return result
+// }
+
+// 2 ^ 4 => 2 * 2 ^ 3
+// 2 ^ 3 => 2 * 2 ^ 2
+// 2 ^ 2 => 2 * 2 ^ 1
+// 2 ^ 1 => 2
+
+// power(a, b) => a * power(a, b -1)
+
+function power (a, b) {
+  return b === 1 ? a : a * power(a, b - 1)
 }
 
-/////////////////// 7
+console.log(power(5, 3))
 
-function printFriends (friends) {
-  for (var friend of friends) {
-    console.log(friend)
+////////// 11
+
+function callbackOperation (callback) {
+  var numbers = []
+
+  for (var i = 1; i < arguments.length; i++) {
+    numbers[i - 1] = arguments[i]
+  }
+
+  return callback.apply(null, numbers)
+}
+
+// console.log(callbackOperation(subtraction, 20, 2, 5))
+
+///////// 10
+
+function operation (operator) {
+  var numbers = []
+
+  for (var i = 1; i < arguments.length; i++) {
+    numbers[i - 1] = arguments[i]
+  }
+
+  switch (operator) {
+    case 'sum':
+      return sum.apply(null, numbers)
+
+    case 'sub':
+      return subtraction.apply(null, numbers)
+
+    case 'mul':
+      return multiply.apply(null, numbers)
+
+    case 'div':
+      return division.apply(null, numbers)
   }
 }
 
-// printFriends(person.friends)
+// console.log(operation('sub', 20, 2, 5))
 
-///////////////// 8
+//////// 9
 
-function getFullName (obj) {
-  return obj.firstName + ' ' + obj.lastName
+function sum () {
+  var result = 0
+
+  for (var number of arguments) {
+    result += number
+  }
+
+  return result
 }
 
-console.log(getFullName(person))
+function subtraction (result) {
+  // var result = arguments[0]
+
+  for (var i = 1; i < arguments.length; i++) {
+    result -= arguments[i]
+  }
+
+  return result
+}
+
+function multiply () {
+  var result = 1
+
+  for (var number of arguments) {
+    result *= number
+  }
+
+  return result
+}
+
+function division (result) {
+  for (var i = 1; i < arguments.length; i++) {
+    result /= arguments[i]
+  }
+
+  return result
+}
+
+// console.log(sum(20, 2, 5))
+// console.log(subtraction(20, 2, 5))
+// console.log(multiply(20, 2, 5))
+// console.log(division(20, 2, 5))
+
+// var person = {
+//   firstName: 'Ali',
+//   lastName: 'Mousavi',
+//   age: 33,
+//   family: {
+//     father: 'Mohammad',
+//     mother: 'Hajar'
+//   },
+//   friends: ['Ali', 'Eli', 'Qoli', 'Fati']
+// }
+
+// /////////////////// 7
+
+// function printFriends (friends) {
+//   for (var friend of friends) {
+//     console.log(friend)
+//   }
+// }
+
+// // printFriends(person.friends)
+
+// ///////////////// 8
+
+// function getFullName (obj) {
+//   return obj.firstName + ' ' + obj.lastName
+// }
+
+// console.log(getFullName(person))
 
 ////////////////// 1
 
--- index.js
+++ index.js
@@ -1,227 +1,50 @@
-// var n = parseInt('101', 2)
+// function setCookie (name, value, time, path = '/') {
+//   var date = new Date()
+//   date.setSeconds(date.getSeconds() + time)
 
-// console.log(n)
-// console.log(typeof n)
-
-// // var str = 'salam'
-
-// console.log(str)
-// console.log(str.length)
-
-// str.length = 2
-
-// console.log(str)
-// console.log(str.length)
-
-// var persons = [
-//   { name: 'Milad', age: 31, gender: 'male' },
-//   { name: 'Atefeh', age: 33, gender: 'female' },
-//   { name: 'Maedeh', age: 24, gender: 'female' },
-//   { name: 'Behrooz', age: 29, gender: 'male' },
-//   { name: 'Arlen', age: 25, gender: 'male' }
-// ]
-
-// persons
-//   .sort(function (a, b) {
-//     return a.age - b.age
-//   })
-//   .reverse()
-
-// console.log(persons)
-
-// var arr = [4, 1, 31, 18, 2, 6, 35]
-
-// arr.sort(function (a, b) {
-//   return a - b
-// })
-
-// console.log(arr)
-
-// var sumAges = persons.reduce(function (acc, el) {
-//   return acc + el.age
-// }, 0)
-
-// var average = sumAges / persons.length
-
-// console.log(sumAges)
-// console.log(average)
-
-// var arr = [4, 9, 7, 3, 1, 6]
-
-// var result = arr.reduce(function (acc, el) {
-//   console.log(acc, el)
-//   return acc + el
-// }, 10)
-
-// var result = 0
-
-// arr.forEach(function (el) {
-//   result += el
-// })
-
-// for (var el of arr) {
-//   result = result + el
+//   document.cookie =
+//     name + '=' + value + ';expires=' + date.toUTCString() + ';path=' + path
 // }
 
-// console.log(result)
+// // setCookie('name', 'Ali', 10000)
+// // setCookie('family', 'Mousavi', 10000)
 
-// persons.map(function (el) {
-//   console.log(el.name)
-// })
-
-// var names = persons.map(function (el) {
-//   return el.name
-// })
-
-// console.log(names)
-
-// var arr = [1, 2, 3, 4, 5]
-
-// var newArr = arr.map(function (el) {
-//   return el ** 2
-// })
-
-// console.log(newArr)
-
-// persons.forEach(function (el) {
-//   console.log(el.name)
-// })
-
-// var friends = ['Ali', 'Eli', 'Qoli', 'Fati']
-
-// friends.forEach(function (el) {
-//   console.log(el)
-// })
-
-// for (var friend of friends) {
-//   console.log(friend)
+// function getCookies () {
+//   return Object.fromEntries(
+//     document.cookie.split(';').map(function (el) {
+//       return el.trim().split('=')
+//     })
+//   )
 // }
 
-// for (var i = 0; i < friends.length; i++) {
-//   console.log(friends[i])
+// function getCookie (name) {
+//   return getCookies()[name]
 // }
 
-// var men = persons.filter(function (el) {
-//   return el.gender === 'male'
-// })
-
-// console.log(men)
-
-// var women = persons.filter(function (el) {
-//   return el.gender === 'female'
-// })
-
-// console.log(women)
-
-// var olds = persons.filter(function (el) {
-//   return el.age >= 30
-// })
-
-// var behrooz = persons.find(function (el) {
-//   return el.name === 'Behrooz'
-// })
-
-// console.log(behrooz)
-
-// var hasYoung = persons.some(function (el) {
-//   return el.age < 20
-// })
-
-// console.log(hasYoung)
-
-// var arr = [5, 7, 12, 36, 45, 11]
-
-// var odds = arr.filter(function (el) {
-//   return el % 2
-// })
-
-// var evens = arr.filter(function (el) {
-//   return !(el % 2)
-// })
-
-// console.log(odds)
-// console.log(evens)
-
-// var arr = [1, 9, 3, 5, 11]
-
-// var isOdds = arr.every(function (el) {
-//   return el % 2
-// })
-
-// console.log(isOdds)
-
-// var arr = [4, 6, 2, 0, 8, 20]
-
-// var isEvens = arr.every(function (el, index, arr) {
-//   return el % 2 === 0
-//   //   if (el % 2 === 0) {
-//   //     return true
-//   //   } else {
-//   //     return false
-//   //   }
-// })
-
-// console.log(isEvens)
-
-// var arr = Array.from({ length: 100 }).fill(true, 10, 20)
-// console.log(arr)
-
-// console.log(typeof [])
-// console.log(Array.isArray([]))
-
-// console.log(Array.from('ali'))
-
-// var newArr = Array.from([1, 2, 3], function (el) {
-//   console.log(el)
-//   return el * 2
-// })
-
-// console.log(newArr)
-
-// var newArr = Array.from({ length: 100 }, function (el, index) {
-//   return index + 1
-// })
-
-// console.log(newArr)
-
-// function * generator () {
-//   yield 1
-//   yield 2
-//   yield 3
-//   yield 4
+// function removeCookie (name) {
+//   setCookie(name, '', 0)
 // }
 
-// var it = generator()
+// removeCookie('name')
 
-// console.log(it.next())
-// // console.log(it.next())
-// // console.log(it.next())
-// // console.log(it.next())
-// // console.log(it.next())
+// console.log(getCookie('age'))
 
-// for (var el of it) {
-//   console.log(el)
+// console.log(getCookies())
+
+// function handleClick (event) {
+//   console.log('salam')
+//   console.log(event.target)
+//   event.target.textContent = 'Ali Mousavi'
 // }
 
-// var arr = ['ali', 10, true]
-// console.log(arr.toString())
+// var h2 = document.querySelector('h2')
+// h2.addEventListener('click', handleClick)
 
-// var arr1 = [10]
+// function handleMouse () {
+//   console.log('Mouse over')
+//   h2.style.color = 'red'
+//   h2.removeEventListener('click', handleClick)
+// }
 
-// console.log(arr1)
-// console.log(arr1.length)
-
-// var arr2 = new Array(10)
-
-// console.log(arr2)
-// console.log(arr2.length)
-
-// var arr = ['ali', 10, true]
-// var arr = new Array('ali', 10, true)
-
-// console.log(arr)
-// console.log(arr.length)
-
-// arr.length = 2
-
-// console.log(arr)
+// var a = document.querySelector('a')
+// a.addEventListener('mouseover', handleMouse)
